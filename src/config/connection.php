0d9c795d771a1656cb9747bc073b1054b067cb59
fabriziojuarez <fabriziojuarez79@gmail.com>
2025-04-15 19:02:59 -0500

    creacion de la estructura principal

---
+++ src/config/connection.php
@@ -0,0 +1,27 @@
+<?php
+
+class connectionDb
+{
+    private $hostname = "localhost:3306";
+    private $user = "root";
+    private $pass = "";
+    private $database = "db_api";
+
+    private $con = null;
+
+    public function getConnection()
+    {
+        try {
+            $this->con = mysqli_connect(
+                $this->hostname,
+                $this->user,
+                $this->pass,
+                $this->database
+            );
+            echo "conexion hecha";
+            return $this->con;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
